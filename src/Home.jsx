af428cbdb77aaddd068a1cfb289203e90d805ccc
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-03 16:23:14 -0800

    fixed manifest error

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Home = () => {
     const position = [51.505, -0.09];
