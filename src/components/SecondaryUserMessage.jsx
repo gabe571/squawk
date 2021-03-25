d208a826b77591ff39760c7834f7234a74ae38c7
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-25 06:37:05 -0700

    SecondaryUserMessage logic

--- src/components/SecondaryUserMessage.jsx
+++ src/components/SecondaryUserMessage.jsx
@@ -1,10 +1,31 @@
-const SecondaryUserMessage = () => {
+const SecondaryUserMessage = (lastMessage, message) => {
+    //check if first message by user
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
 
     return (
-        <div>
-            Secondary User
-        </div>
-    )
-}
+        <div className="message-row">
+        {isFirstMessageByUser && (
+          <div
+            className="message-avatar"
+            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+          />
+        )}
+        {message.attachments && message.attachments.length > 0
+          ? (
+            <img
+              src={message.attachments[0].file}
+              alt="message-attachment"
+              className="message-image"
+              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            />
+          )
+          : (
+            <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+              {message.text}
+            </div>
+          )}
+      </div>
+    );
+  };
 
 export default SecondaryUserMessage
