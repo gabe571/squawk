a60088fbe3676faee7d0a387fcfe534cb6e71457
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-25 05:21:42 -0700

    chatEngine/css

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ const App = () => {
     return (
         <ChatEngine
             height='100vh'
-            projectID=''
-            userName=''
-            userSecret=''
+            projectID='7d0c60bc-9fb5-4e8d-80d1-e3331b0ebe05'
+            userName='gabe571'
+            userSecret='1234'
         />
     )
 }
+
+export default App;
