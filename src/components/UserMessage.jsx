12fdce67d6ddcd256440bc35da5abfc58b1d8221
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-25 06:20:29 -0700

    UserMessage Logic

--- src/components/UserMessage.jsx
+++ src/components/UserMessage.jsx
@@ -1,8 +1,18 @@
-const UserMessage = () => {
-
+const UserMessage = ({ message }) => {
+    //check if message is an image
+    if(message?.attachments?.length > 0) {
+        return (
+            <img 
+            src={message.attachments[0].file}
+            alt='message-attachment'
+            className='message-image'
+            style={{ float: 'right' }}
+            />
+        )
+    }
     return (
-        <div>
-           User Message
+        <div className='message' style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3b2a50'}}>
+           {message.text}
         </div>
     )
 }
